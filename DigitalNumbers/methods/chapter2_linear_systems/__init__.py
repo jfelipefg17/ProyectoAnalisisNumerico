5721aa992e0b849b3aef320cfcd36f931dedae88
Polar-M <jeromesa2006@gmail.com>
2026-04-19 14:09:11 -0500

    Update __init__.py

--- DigitalNumbers/methods/chapter2_linear_systems/__init__.py
+++ DigitalNumbers/methods/chapter2_linear_systems/__init__.py
@@ -1,3 +1,4 @@
 # Chapter 2 - Linear Systems methods
 from .naive_gaussian_elimination import naive_gaussian_elimination
 from .gaussian_elimination_PP import gaussian_elimination_PP
+from .gaussian_elimination_TP import gaussian_elimination_TP
