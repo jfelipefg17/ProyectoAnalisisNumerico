c53f96d69b33961408dbfb8618199311a883d866
Luciana934 <lucianapineda.0904@gmail.com>
2026-04-20 11:23:43 -0500

    secant.py

---
+++ DigitalNumbers/methods/chapter1_single_variable/secant.py
@@ -0,0 +1,40 @@
+def secant(f, x0, x1, tol, N):
+    import pandas as pd
+
+    data = []
+    error = float("inf")
+
+    for k in range(1, N+1):
+        if f(x1) - f(x0) == 0:
+            return {
+                "table": pd.DataFrame(data),
+                "iters": k,
+                "root": x1,
+                "error": error,
+                "converged": False
+            }
+
+        x2 = x1 - f(x1)*(x1 - x0)/(f(x1) - f(x0))
+        error = abs(x2 - x1)
+
+        data.append([k, x0, x1, x2, f(x2), error])
+
+        if error < tol:
+            return {
+                "table": pd.DataFrame(data, columns=["k","x0","x1","x2","f(x2)","error"]),
+                "iters": k,
+                "root": x2,
+                "error": error,
+                "converged": True
+            }
+
+        x0 = x1
+        x1 = x2
+
+    return {
+        "table": pd.DataFrame(data, columns=["k","x0","x1","x2","f(x2)","error"]),
+        "iters": N,
+        "root": x2,
+        "error": error,
+        "converged": False
+    }
