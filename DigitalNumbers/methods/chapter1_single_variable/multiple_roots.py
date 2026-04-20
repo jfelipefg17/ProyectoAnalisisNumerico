aafc71118ceafe81abd7c04f4a519d63f5486e8f
Luciana934 <lucianapineda.0904@gmail.com>
2026-04-20 11:19:23 -0500

    multiple_roots.py

--- DigitalNumbers/methods/chapter1_single_variable/multiple_roots.py
+++ DigitalNumbers/methods/chapter1_single_variable/multiple_roots.py
@@ -1,26 +1,45 @@
 def multiple_roots(f, df, d2f, x0, tol, N):
-  print("k   x   f(x)   f'(x)   error")
+    import pandas as pd
 
-  for k in range(1, N+1):
-    fx = f(x0)
-    dfx = df(x0)
-    d2fx = d2f(x0)
-  
-    denom = dfx**2 - fx*d2fx
-  
-    if denom == 0:
-      print("Error: división por cero")
-      return None
-    
-    x1 = x0 - (fx * dfx) / denom
-    error = abs(x1 - x0)
-  
-    print(k, x0, fx, dfx, error)
-  
-    if error < tol:
-      print("Aproximación de la raíz:", x1)
-      return x1
-    x0 = x1
-  
-  print("El método no covergió")
-  return x1
+    data = []
+    error = float("inf")
+
+    for k in range(1, N+1):
+        fx = f(x0)
+        dfx = df(x0)
+        d2fx = d2f(x0)
+
+        denom = dfx**2 - fx*d2fx
+
+        if denom == 0:
+            return {
+                "table": pd.DataFrame(data),
+                "iters": k,
+                "root": x0,
+                "error": error,
+                "converged": False
+            }
+
+        x1 = x0 - (fx * dfx) / denom
+        error = abs(x1 - x0)
+
+        data.append([k, x0, fx, dfx, error])
+
+        if error < tol:
+            return {
+                "table": pd.DataFrame(data, columns=["k", "x", "f(x)", "f'(x)", "error"]),
+                "iters": k,
+                "root": x1,
+                "error": error,
+                "converged": True
+            }
+
+        x0 = x1
+
+    return {
+        "table": pd.DataFrame(data, columns=["k", "x", "f(x)", "f'(x)", "error"]),
+        "iters": N,
+        "root": x1,
+        "error": error,
+        "converged": False
+    }
