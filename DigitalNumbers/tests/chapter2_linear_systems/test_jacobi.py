85b31225f3cd328f15a6cc8788f9af6e467c5fff
Luciana934 <lucianapineda.0904@gmail.com>
2026-05-12 11:06:16 -0500

    Update test_jacobi.py

--- DigitalNumbers/tests/chapter2_linear_systems/test_jacobi.py
+++ DigitalNumbers/tests/chapter2_linear_systems/test_jacobi.py
@@ -1,1 +1,30 @@
+# Test for Jacobi Method
 
+A = np.array([
+    [4, -1, 0, 3],
+    [1, 15.5, 3, 8],
+    [0, -1.3, -4, 1.1],
+    [14, 5, -2, 30]
+], dtype=float)
+
+b = np.array([1, 1, 1, 1], dtype=float)
+
+x0 = np.array([0, 0, 0, 0], dtype=float)
+
+tol = 1e-7
+Nmax = 100
+
+# Select norm type
+norm_type = 2
+
+# Run method
+x, iterations, error = jacobi(A, b, x0, tol, Nmax, norm_type)
+
+# Results
+print("Final Results:")
+
+print("x =", x)
+
+print("Iterations =", iterations)
+
+print("Error =", error)
