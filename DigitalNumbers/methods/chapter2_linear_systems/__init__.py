5eaa3a60e0b1109e14da5e49e3352dd319292f75
Polar-M <jeromesa2006@gmail.com>
2026-05-11 23:50:48 -0500

    Update __init__.py

--- DigitalNumbers/methods/chapter2_linear_systems/__init__.py
+++ DigitalNumbers/methods/chapter2_linear_systems/__init__.py
@@ -3,3 +3,4 @@ from .naive_gaussian_elimination import naive_gaussian_elimination
 from .gaussian_elimination_PP    import gaussian_elimination_PP
 from .gaussian_elimination_TP    import gaussian_elimination_TP
 from .LU_simple import LU_simple
+from .LU_partial_pivoting import LU_partial_pivoting
