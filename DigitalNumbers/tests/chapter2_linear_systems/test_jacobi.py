0608c7dd9218f38d29b579ad3a6c88ee78b2c09b
Luciana934 <lucianapineda.0904@gmail.com>
2026-05-12 11:07:30 -0500

    Create test_jacobi.py

--- DigitalNumbers/tests/chapter2_linear_systems/test_jacobi.py
+++ DigitalNumbers/tests/chapter2_linear_systems/test_jacobi.py
@@ -4,8 +4,7 @@ A = np.array([
     [4, -1, 0, 3],
     [1, 15.5, 3, 8],
     [0, -1.3, -4, 1.1],
-    [14, 5, -2, 30]
-], dtype=float)
+    [14, 5, -2, 30]], dtype=float)
 
 b = np.array([1, 1, 1, 1], dtype=float)
 
