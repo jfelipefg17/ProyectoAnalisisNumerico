1813be5bb204437140225b1e31ed0455d002ccdc
Polar-M <jeromesa2006@gmail.com>
2026-05-12 02:03:30 -0500

    Update test_LU_simple.py

--- DigitalNumbers/tests/chapter2_linear_systems/test_LU_simple.py
+++ DigitalNumbers/tests/chapter2_linear_systems/test_LU_simple.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 import numpy as np
 
 from methods.chapter2_linear_systems.LU_simple import LU_simple
-from helpers.print_helpers import print_lu_result
+from tests.print_helpers import print_lu_result
 
 # ──────────────────────────────────────────────
 # Test 
